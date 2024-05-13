8329897ef99149875f7a2729839c2bfae310b708
Bill Anthony <nino.billanthony@gmail.com>
2024-05-12 21:15:54 -0400

    Update Connection

--- Resources/Connection.py
+++ Resources/Connection.py
@@ -2,3 +2,20 @@ import sqlite3
 
 db = sqlite3.connect("Resources\SIEIDB.db")
 cur = db.cursor()
+
+def insert_course(id, name, op, format, xx, yy):
+  db = sqlite3.connect("Resources\SIEIDB.db")
+  cur = db.cursor()
+  cur.execute('INSERT INTO Courses (id, name, duration, format, languaje, price) VALUES(?,?,?,?,?,?)',
+              (id, name, op, format, xx, yy))
+  db.commit()
+  db.close()
+  
+
+def id_exist(id):
+  db = sqlite3.connect("Resources\SIEIDB.db")
+  cur = db.cursor()
+  cur.execute('SELECT COUNT(*) FROM Courses WHERE id = ?', (id,))
+  result = cur.fetchone()
+  db.close()
+  return result[0] > 0
