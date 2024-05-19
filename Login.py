08f1c790b3dcccf1753d209e58e278c64fb9e3c9
Bill Anthony <nino.billanthony@gmail.com>
2024-05-19 15:12:30 -0400

    Update Login
    Tags were added for the identification of the code.

--- Login.py
+++ Login.py
@@ -1,38 +1,45 @@
+
+# Libraries
 from customtkinter import *
 from tkinter import messagebox
 
-import sqlite3
+# Communicating with SQLite3 to get the login data from the database
+import Resources.Connection
 
-db = sqlite3.connect("Resources\SIEIDB.db")
-cur = db.cursor()
-
-set_appearance_mode ("dark")
-set_default_color_theme ("blue")
-
+# User validator to enter the system
 def login():
   user = em.get()
   psw = ps.get()
-  cur.execute ('SELECT * FROM "Usuarios-Sis" WHERE Usuario = ? AND PSW = ?', [user, psw])
-  if cur.fetchall():
+  Resources.Connection.loginc(user, psw)
+  if Resources.Connection.cur.fetchall():
     messagebox.showinfo ('Login', 'Acceso permitido')
   else:
     messagebox.showerror ('Error','Acceso denegado')
 
+# Defined appearance
+set_appearance_mode ("dark")
+set_default_color_theme ("blue")
+
+# Format of the window interface
 main = CTk ()
 main.title ("CTK - Login")
 main.geometry ("500x500")
 
+# Format of the frame that forms the main body of the window
 user = CTkFrame (main)
 user.pack (expand = True, fill = "both", padx = 20, pady = 60 )
 
+# Title
 CTkLabel (user, text = "Login system", font= ("Roboto", 26)).pack (pady = 20)
 
+# Data entry for the login
 em = CTkEntry (user, placeholder_text = "Email", width=250)
 em.pack ()
 
 ps= CTkEntry (user, placeholder_text = "Password", width=250)
 ps.pack (pady = 15)
 
-CTkButton (user, text = "Login", command=login).pack ()
+# login button
+CTkButton (user, text = "login", command=login).pack ()
 
 main.mainloop ()
