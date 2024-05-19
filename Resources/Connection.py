8964e4ff312bf127c65b7643681d7de56383106b
Bill Anthony <nino.billanthony@gmail.com>
2024-05-19 15:08:48 -0400

    Update DB and Connection
    Some records were added to the database and also for the database connector and application new functions were created. (Ademas desde este punto todo lo referente a los Commits sera en ingles)

--- Resources/Connection.py
+++ Resources/Connection.py
@@ -3,19 +3,27 @@ import sqlite3
 db = sqlite3.connect("Resources\SIEIDB.db")
 cur = db.cursor()
 
-def insert_course(id, name, op, format, xx, yy):
+def loginc(user, psw):
   db = sqlite3.connect("Resources\SIEIDB.db")
   cur = db.cursor()
-  cur.execute('INSERT INTO Courses (id, name, duration, format, languaje, price) VALUES(?,?,?,?,?,?)',
-              (id, name, op, format, xx, yy))
+  cur.execute ('SELECT * FROM "Usuarios-Sis" WHERE Usuario = ? AND PSW = ?', [user, psw])
+
+def insert_course(id, name, model, serial, stat):
+  db = sqlite3.connect("Resources\SIEIDB.db")
+  cur = db.cursor()
+  cur.execute('INSERT INTO "BO" (id, Nombre, Modelo, Serial, Estado) VALUES(?,?,?,?,?)',
+              (id, name, model, serial, stat))
   db.commit()
   db.close()
   
-
 def id_exist(id):
   db = sqlite3.connect("Resources\SIEIDB.db")
   cur = db.cursor()
-  cur.execute('SELECT COUNT(*) FROM Courses WHERE id = ?', (id,))
+  cur.execute('SELECT COUNT(*) FROM BO WHERE id = ?', [id])
   result = cur.fetchone()
   db.close()
   return result[0] > 0
+
+def search(val, stat):
+  cur.execute('SELECT * FROM BO WHERE id = ? OR Nombre LIKE ? AND Estado = ?', [val, '%'+val+'%', stat])
+
