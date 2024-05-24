05e45646a572752f02b2e1b868080e923c45aa7a
Bill Anthony <nino.billanthony@gmail.com>
2024-05-23 21:09:21 -0400

    Update for the graphical interface
    Updating the format of the windows.

--- Dataview.py
+++ Dataview.py
@@ -18,13 +18,11 @@ app.geometry ('850x650')
 
 
 status = StringVar()
-options = ['Operativo', 'Inoperativo', 'Estado']
+options = ['Operativo', 'Inoperativo']
 
 stat_options = CTkComboBox(app,text_color='#000', fg_color='#fff',dropdown_hover_color='#7d01b2', button_color='#7d01b2',button_hover_color='#7d01b2',border_color="#7d01b2", width=150, variable=status, values=options, state='readonly')
 stat_options.set('Operativo')
-stat_options.place(x=60, y=400)
-
-
+stat_options.place(x=460, y=15)
 
 def find():
 
@@ -42,14 +40,15 @@ def find():
 
 frame = tk.Frame(app)
 
-title_label = CTkLabel(app, text='Computadoras registradas', text_color='#fff',bg_color='#292933', font = ('Roboto',20, 'bold'))
-title_label.place(x=425, y=15)
+title_label = CTkLabel(app, text='Computadoras registradas', text_color='#fff', font = ('Roboto',20, 'bold'))
+title_label.place(x=350, y=45)
 
-entry_search = CTkEntry(app, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
-entry_search.place(x=50, y=500)
+entry_search = CTkEntry(app, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=300)
+entry_search.place(x=30, y=15)
 
 button_search = CTkButton(app, command=find, text_color='#fff', text='Buscar',fg_color='#02ab10',hover_color='#02920D', bg_color='#292933', cursor='hand2',corner_radius=5, width=100)
-button_search.place(x=60, y=550)
+button_search.place(x=350, y=15)
+
 
 trv = ttk.Treeview(app, height=22, show="headings")
 
@@ -86,13 +85,64 @@ scrollbary.place(x=820, y=50, width=20, height=485)
 
 scrollbarx = ttk.Scrollbar(app, orient=tk.HORIZONTAL, command=trv.xview)
 trv.configure(xscroll=scrollbarx.set)
-scrollbarx.place(x=300, y=515, width=520, height=20)
+scrollbarx.place(x=30, y=515, width=789, height=20)
+
+trv.place(x=30, y=50, width=789, height=465)
 
 button_search ['command'] = find
 
 find()
 
-trv.place(x=300, y=50, width=519, height=465)
+# def bmo():
+#     frame = tk.Frame(app)
+
+#     title_label = CTkLabel(app, text='Computadoras registradas', text_color='#fff',bg_color='#292933', font = ('Roboto',20, 'bold'))
+#     title_label.place(x=425, y=15)
+
+#     entry_search = CTkEntry(app, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
+#     entry_search.place(x=50, y=15)
+
+#     button_search = CTkButton(app, command=find, text_color='#fff', text='Buscar',fg_color='#02ab10',hover_color='#02920D', bg_color='#292933', cursor='hand2',corner_radius=5, width=100)
+#     button_search.place(x=60, y=15)
+
+#     trv = ttk.Treeview(app, height=22, show="headings")
+
+#     trv.configure(columns=(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11))
+
+#     trv.column(1, stretch=NO, width=20)
+#     trv.column(2, stretch=NO, width=100)
+#     trv.column(3, stretch=NO, width=100)
+#     trv.column(4, stretch=NO, width=100)
+#     trv.column(5, stretch=NO, width=100)
+#     trv.column(6, stretch=NO, width=100)
+#     trv.column(7, stretch=NO, width=100)
+#     trv.column(8, stretch=NO, width=100)
+#     trv.column(9, stretch=NO, width=100)
+#     trv.column(10, stretch=NO, width=100)
+#     trv.column(11, stretch=NO, width=100)
+
+#     trv.heading(1, text='ID',anchor=tk.CENTER)
+#     trv.heading(2, text='Nombre',anchor=tk.CENTER)
+#     trv.heading(3, text='Modelo',anchor=tk.CENTER)
+#     trv.heading(4, text='Serial',anchor=tk.CENTER)
+#     trv.heading(5, text='Color', anchor=tk.CENTER)
+#     trv.heading(6, text='Color-MB', anchor=tk.CENTER)
+#     trv.heading(7, text='CPU', anchor=tk.CENTER)
+#     trv.heading(8, text='RAM', anchor=tk.CENTER)
+#     trv.heading(9, text='HDDvsSSD', anchor=tk.CENTER)
+#     trv.heading(10, text='Estado', anchor=tk.CENTER)
+#     trv.heading(11, text='Usuario', anchor=tk.CENTER)
+
+#     scrollbary = ttk.Scrollbar(app, orient=tk.VERTICAL, command=trv.yview)
+#     trv.configure(yscroll=scrollbary.set)
+#     trv.configure(selectmode="extended")
+#     scrollbary.place(x=820, y=50, width=20, height=485)
+
+#     scrollbarx = ttk.Scrollbar(app, orient=tk.HORIZONTAL, command=trv.xview)
+#     trv.configure(xscroll=scrollbarx.set)
+#     scrollbarx.place(x=300, y=515, width=520, height=20)
+
+#     trv.place(x=300, y=50, width=519, height=465)
 
 
 app.mainloop ()
--- Main.py
+++ Main.py
@@ -5,8 +5,10 @@ from tkinter import *
 from tkinter import messagebox
 
 # Communicating with SQLite3 to get the login data from the database
-import Resources.Connection 
+import Resources.Connection
 
+# def Mainv():
+  
 # Database manipulators
 def new_course():
   id_entry.delete(0,END)
@@ -20,14 +22,21 @@ def submit_course():
   name = name_entry.get()
   model = model_entry.get()
   serial = serial_entry.get()
+  color = color_entry.get()
+  colormb = colorMB_entry.get()
+  cpu = cpu_entry.get()
+  ram = ram_entry.get()
+  disk = HDDvsSDD_entry.get()
   stat = status.get()
+  fei = fechaenti_entry.get()
+  fsi = fechasali_entry.get()
   try:
-    if not (id and name and model and serial and stat):
+    if not (id and name and model and serial and color and colormb and cpu and ram and disk and stat and fei and fsi):
       messagebox.showerror('Error', 'Se debe llenar las celdas.')
     elif Resources.Connection.id_exist(id):
       messagebox.showerror('Error', 'ID ya existe.')
     else:
-      Resources.Connection.insert_course(id, name, model, serial, stat)
+      Resources.Connection.insert_course(id, name, model, serial, color, colormb, cpu, ram, disk, stat, fei, fsi)
       messagebox.showinfo('Success', 'La información fue registrado')
   except:
     messagebox.showerror('Error','Ocurrio un error.')
@@ -57,32 +66,75 @@ id_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', bord
 id_entry.place(x=50, y=45)
 
 name_label = CTkLabel(frame1, font=font2, text='Nombre:',text_color='#fff',bg_color='#292933')
-name_label.place(x=50, y=90)
+name_label.place(x=250, y=15)
 
 name_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
-name_entry.place(x=50, y=120)
+name_entry.place(x=250, y=45)
+
+serial_label = CTkLabel(frame1, font=font2, text='Serial:',text_color='#fff',bg_color='#292933')
+serial_label.place(x=445, y=15)
+
+serial_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
+serial_entry.place(x=445, y=45)
+
+model_label = CTkLabel(frame1, font=font2, text='Modelo:',text_color='#fff',bg_color='#292933')
+model_label.place(x=50, y=90)
+
+model_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
+model_entry.place(x=50, y=115)
+
+color_label = CTkLabel(frame1, font=font2, text='Color:',text_color='#fff',bg_color='#292933')
+color_label.place(x=250, y=90)
+
+color_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
+color_entry.place(x=250, y=115)
+
+colorMB_label = CTkLabel(frame1, font=font2, text='Color de la placa madre:',text_color='#fff',bg_color='#292933')
+colorMB_label.place(x=445, y=90)
+
+colorMB_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
+colorMB_entry.place(x=445, y=115)
+
+cpu_label = CTkLabel(frame1, font=font2, text='CPU:',text_color='#fff',bg_color='#292933')
+cpu_label.place(x=50, y=160)
+
+cpu_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
+cpu_entry.place(x=50, y=190)
+
+ram_label = CTkLabel(frame1, font=font2, text='RAM:',text_color='#fff',bg_color='#292933')
+ram_label.place(x=250, y=160)
+
+ram_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
+ram_entry.place(x=250, y=190)
+
+HDDvsSDD_label = CTkLabel(frame1, font=font2, text='Unidad de almacenamiento:',text_color='#fff',bg_color='#292933')
+HDDvsSDD_label.place(x=50, y=230)
+
+HDDvsSDD_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
+HDDvsSDD_entry.place(x=50, y=260)
 
 stat_label = CTkLabel(frame1, font=font2, text='Estado:',text_color='#fff',bg_color='#292933')
-stat_label.place(x=445, y=15)
+stat_label.place(x=445, y=160)
 
 status = StringVar()
 options = ['Operativo', 'Inoperativo']
 
 stat_options = CTkComboBox(frame1,font=font2,text_color='#000', fg_color='#fff',dropdown_hover_color='#7d01b2', button_color='#7d01b2',button_hover_color='#7d01b2',border_color="#7d01b2", width=150, variable=status, values=options, state='readonly')
 stat_options.set('Operativo')
-stat_options.place(x=445, y=45)
+stat_options.place(x=445, y=190)
 
-serial_label = CTkLabel(frame1, font=font2, text='Serial:',text_color='#fff',bg_color='#292933')
-serial_label.place(x=50, y=160)
+fechaenti_label = CTkLabel(frame1, font=font2, text='Fecha de entrada a la entidad:',text_color='#fff',bg_color='#292933')
+fechaenti_label.place(x=50, y=300)
 
-serial_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
-serial_entry.place(x=50, y=190)
+fechaenti_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
+fechaenti_entry.place(x=50, y=330)
 
-model_label = CTkLabel(frame1, font=font2, text='Modelo:',text_color='#fff',bg_color='#292933')
-model_label.place(x=50, y=220)
+fechasali_label = CTkLabel(frame1, font=font2, text='Fecha de salidad de la entidad:',text_color='#fff',bg_color='#292933')
+fechasali_label.place(x=50, y=360)
 
-model_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
-model_entry.place(x=50, y=250)
+fechasali_entry = CTkEntry(frame1, font=font2, text_color='#000', fg_color='#fff', border_color='#B2016C', border_width=2, width=150)
+fechasali_entry.place(x=50, y=390)
+
 
 submit_button = CTkButton(app, command=submit_course ,font=font2, text_color='#fff', text='Guardar',fg_color='#02ab10',hover_color='#02920D', bg_color='#292933', cursor='hand2',corner_radius=5, width=100)
 submit_button.place(x=200, y=550)
